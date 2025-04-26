06f09e09385213c8b481f945975d0b6afa3b3129
aviv <avivyona17@gmail.com>
2025-04-26 21:04:50 +0300

    fix overflow

--- Yona-Marble-Frontend/src/components/Catalog/Catalog.tsx
+++ Yona-Marble-Frontend/src/components/Catalog/Catalog.tsx
@@ -1,6 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import { useState } from "react";
+import { useMediaQuery } from "@mui/material";
 
 interface ItemData {
   img: string;
@@ -11,61 +13,99 @@ interface Props {
 }
 
 export const Catalog = (props: Props) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const isSmall = useMediaQuery("(max-width:600px)");
+  const containerWidth = isSmall ? "90vw" : "70vw";
+  const containerHeight = isSmall ? "25vh" : "40vh";
+
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div
-        style={{
-          width: "70vw",
-          height: "40vh",
-        }}
-      >
-        <Swiper
-          modules={[Navigation, Pagination, EffectCoverflow]}
-          navigation
-          pagination={{ clickable: true }}
-          loop={true}
-          effect="coverflow"
-          coverflowEffect={{
-            rotate: 30,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          spaceBetween={30}
+    <>
+      {selectedImage && (
+        <div
           style={{
-            width: "100%",
-            height: "100%",
-            borderRadius: "10px",
-            overflow: "hidden",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "rgba(0,0,0,0.8)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 1000,
+            cursor: "pointer",
+          }}
+          onClick={() => setSelectedImage(null)}
+        >
+          <img
+            src={selectedImage}
+            alt=""
+            style={{
+              maxWidth: "90vw",
+              maxHeight: "90vh",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+      )}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{
+            width: containerWidth,
+            height: containerHeight,
+            position: "relative",
           }}
         >
-          {props.itemData.map((item, index) => (
-            <SwiperSlide key={index}>
-              <div
-                style={{
-                  position: "relative",
-                  width: "100%",
-                  height: "100%",
-                  overflow: "hidden",
-                }}
-              >
-                <img
-                  src={item.img}
-                  alt=""
+          <Swiper
+            modules={[Navigation, Pagination, EffectCoverflow]}
+            navigation
+            pagination={{ clickable: true }}
+            loop={true}
+            effect="coverflow"
+            coverflowEffect={{
+              rotate: 30,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            spaceBetween={30}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: "10px",
+              overflow: "hidden",
+            }}
+          >
+            {props.itemData.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div
                   style={{
+                    position: "relative",
                     width: "100%",
                     height: "100%",
-                    objectFit: "cover",
+                    overflow: "hidden",
                   }}
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+                >
+                  <img
+                    src={item.img}
+                    alt=""
+                    onClick={() => setSelectedImage(item.img)}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      cursor: "pointer",
+                    }}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
