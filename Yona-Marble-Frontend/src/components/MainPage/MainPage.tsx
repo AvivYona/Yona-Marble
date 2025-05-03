bb7153b3af675d11d39f3ca509cf37c98af45af9
aviv <avivyona17@gmail.com>
2025-05-03 14:51:04 +0300

    fix positioning

--- Yona-Marble-Frontend/src/components/MainPage/MainPage.tsx
+++ Yona-Marble-Frontend/src/components/MainPage/MainPage.tsx
@@ -22,7 +22,7 @@ const HeroSection = styled(Box)(() => ({
 export const MainPage = () => {
   const isSmallScreen = useMediaQuery("(max-width:720px)");
   return (
-    <Box sx={{ height: "94vh" }}>
+    <Box sx={{ height: "90vh" }}>
       <HeroSection>
         <Box sx={{ position: "relative", zIndex: 1 }}>
           <Box sx={{ display: "flex", justifyContent: "center", mb: 3 }}>
