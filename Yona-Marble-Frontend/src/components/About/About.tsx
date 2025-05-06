dbb279a64c9d320b4c35b1af8c8c239cd87653b9
aviv <avivyona17@gmail.com>
2025-05-06 16:23:15 +0300

    fix animation

--- Yona-Marble-Frontend/src/components/About/About.tsx
+++ Yona-Marble-Frontend/src/components/About/About.tsx
@@ -26,7 +26,7 @@ export const About = () => {
           initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          viewport={{ once: true, amount: 0.6 }}
+          viewport={{ once: true, amount: 0.4 }}
         >
           <Typography
             variant="h5"
